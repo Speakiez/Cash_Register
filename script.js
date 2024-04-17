fe91e925091bd72ea6ca44852fdef104c0bc3812
Speakiez <aerholgocalin57@gmail.com>
2024-04-17 12:27:49 +0800

    Add actually good code for getting valid denoms

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Variable Declarations //
+
 const inputContainer = document.querySelector(".input-container");
 const inputElement = document.getElementById("input");
 const outputContainer = document.querySelector(".output-container");
@@ -8,12 +10,13 @@ const cidElement = document.querySelector(".cid-element");
 
 let cash;
 let change;
+let outputMessage;
 let price = 19.5;
 let cid = [
     ["PENNY", 0.01], 
     ["NICKEL", 0], 
     ["DIME", 0], 
-    ["QUARTER", 0], 
+    ["QUARTER", 0.5], 
     ["ONE", 1], 
     ["FIVE", 0], 
     ["TEN", 0], 
@@ -21,16 +24,68 @@ let cid = [
     ["ONE HUNDRED", 0]
 ];
 
+// Function Declarations //
+
+const cleanFloat = float => parseFloat((float).toFixed(2));
+
 const updateDrawerDisplay = () => {
     cidElement.innerHTML = `<strong>Change in drawer: </strong><br/>` + cid
-        .map((denominator) => `${denominator[0]}: $${denominator[1]}`)
+        .map((denomination) => `${denomination[0]}: $${denomination[1]}`)
         .join(`<br/>`);
 };
 
-window.addEventListener("load", () => {
-    priceSpan.textContent = `
-    Price: $${price}
-    `;
+const validateUserInput = (userInput) => {
+    if (isNaN(userInput)) {
+        alert("Please input a valid number");
+        return;
+    }
 
+    cash = userInput;
+    change = cleanFloat(cash - price);
+
+    if (change === 0) {
+        outputMessage = "No change due - customer paid with exact cash";
+    } else if (!getValidDenominations(change)) {
+        outputMessage = "Status: INSUFFICIENT_FUNDS";
+    } else {
+        outputMessage = "Test";
+    }
+    
+    console.log(cash);
+    console.log(change);
+    console.log(outputMessage);
+
+    inputElement.value = "";
+};
+
+const getValidDenominations = (changeCopy) => {
+    const validDenominations = cid.toReversed().map((denomination) => {
+        if (
+            denomination[1] <= changeCopy && 
+            denomination[1] > 0
+        ) {
+            changeCopy -= denomination[1];
+            return denomination;
+        }
+    })
+    .filter(array => array);
+
+    const totalDenominations = 
+        validDenominations.slice()
+        .reduce((totalDenom, currentDenom) => totalDenom + currentDenom[1], 0);
+    
+    if (totalDenominations >= change) {
+        return validDenominations;
+    }
+};
+
+// Event Handling //
+
+purchaseButton.addEventListener("click", () => {
+    validateUserInput(parseFloat(inputElement.value));
+});
+
+window.addEventListener("load", () => {
+    priceSpan.textContent = `Price: $${price}`;
     updateDrawerDisplay();
 });
