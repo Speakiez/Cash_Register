88131914e050ddda1106791407a069e6bcb92fd9
Speakiez <aerholgocalin57@gmail.com>
2024-04-17 10:59:59 +0800

    Start over because code was terrible

--- script.js
+++ script.js
@@ -6,127 +6,31 @@ const purchaseButton = document.querySelector(".purchase-button");
 const priceSpan = document.querySelector(".price-indicator");
 const cidElement = document.querySelector(".cid-element");
 
-const baseCID = [
-    ["PENNY", 0.01],
-    ["NICKEL", 0.05],
-    ["DIME", 0.1],
-    ["QUARTER", 0.25],
-    ["ONE", 1],
-    ["FIVE", 5],
-    ["TEN", 10],
-    ["TWENTY", 20],
-    ["ONE HUNDRED", 100]
-]
-
 let cash;
-let price = 3.26;
+let change;
+let price = 19.5;
 let cid = [
-    ["PENNY", 1.01],
-    ["NICKEL", 2.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100]
+    ["PENNY", 0.01], 
+    ["NICKEL", 0], 
+    ["DIME", 0], 
+    ["QUARTER", 0], 
+    ["ONE", 1], 
+    ["FIVE", 0], 
+    ["TEN", 0], 
+    ["TWENTY", 0], 
+    ["ONE HUNDRED", 0]
 ];
 
-const cleanFloat = float => parseFloat((float).toFixed(2));
-
-const getTotalCID = () => {
-    return cleanFloat(
-        cid.slice()
-        .reduce(
-            (total, currentArray) => total + currentArray[1]
-        , 0)
-    );
-}
-
-const changeArrayToString = (changeArray) => {
-    return changeArray
-        .map((currentArray) => `${currentArray[0]}: $${currentArray[1]}`)
-        .join("<br>");
+const updateDrawerDisplay = () => {
+    cidElement.innerHTML = `<strong>Change in drawer: </strong><br/>` + cid
+        .map((denominator) => `${denominator[0]}: $${denominator[1]}`)
+        .join(`<br/>`);
 };
-
-const updateCashInRegisterDisplay = () => {
-    const cashInRegisterStr = changeArrayToString(cid); 
-    cidElement.innerHTML = `
-    <strong>Change in drawer:<br/></strong>
-    ${cashInRegisterStr}
-    `;
-}
-
-const updateChangeDisplay = (cashFromRegister) => {
-    const cashFromRegisterStr = changeArrayToString(cashFromRegister);
-    const totalCID = getTotalCID();
-
-    if (cash === totalCID + price) {
-        outputSpan.innerHTML = `Status: CLOSED <br/>` + cashFromRegisterStr;
-    } else {
-        outputSpan.innerHTML = `Status: OPEN <br/>` + cashFromRegisterStr;
-    }
-};
-
-const updateCashInRegister = (change) => {
-    for (const changeArr of change) {
-        for (const cidArr of cid) {
-            if (cidArr.includes(changeArr[0])) {
-                cidArr[1] = cleanFloat(cidArr[1] - changeArr[1]);
-            }
-        } 
-    }
-}
-
-const takeCashfromRegister = (change) => {
-    const cashFromRegister = [];
-
-    for (const currentDenom of baseCID.toReversed()) {
-        if (cleanFloat(change - currentDenom[1]) >= 0) {
-            const exactDenom = currentDenom.slice();
-
-            while (
-                (exactDenom[1] + currentDenom[1] - 0.01) < change 
-             && (exactDenom[1] + currentDenom[1] - 0.01) < cid[baseCID.indexOf(currentDenom)][1]) {
-                exactDenom[1] = cleanFloat(exactDenom[1] + currentDenom[1]);
-            }
-            if (exactDenom[1] > cid.at(baseCID.indexOf(currentDenom))[1]) {
-                console.log(exactDenom[1]);
-            } else {
-                cashFromRegister.push(exactDenom);                 
-                change = cleanFloat(change - exactDenom[1]);
-            }
-        }
-    }
-
-    return cashFromRegister;
-};
-
-purchaseButton.addEventListener("click", () => {
-    if (!inputElement.value) {
-        alert("Please input a valid number");
-        return;
-    } else if (Number(inputElement.value) < price) {
-        alert("Customer does not have enough money to purchase the item");
-        return;
-    } else if (Number(inputElement.value) === price) {
-        outputSpan.textContent = "No change due - customer paid with exact cash";
-        return;
-    } else if (Number(inputElement.value) > cleanFloat(getTotalCID() + price)) {
-        outputSpan.textContent = "Status: INSUFFICIENT_FUNDS";
-        return;
-    }
-
-    cash = Number(inputElement.value);
-    let change = takeCashfromRegister(cleanFloat(cash - price));
-
-    updateChangeDisplay(change);
-    updateCashInRegister(change);
-    updateCashInRegisterDisplay();
-    inputElement.value = "";
-});
 
 window.addEventListener("load", () => {
-    priceSpan.textContent += `Price: $${price}`;
-    updateCashInRegisterDisplay();
+    priceSpan.textContent = `
+    Price: $${price}
+    `;
+
+    updateDrawerDisplay();
 });
